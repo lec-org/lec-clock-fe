7aff719cf9b88e8eb01a7c9e772befd6572256b4
yuuki yuuna <2281363234@qq.com>
2023-09-20 21:12:14 +0800

    feat: 项目初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-space>
+    <a-button type="primary" @click="handleClick">Primary</a-button>
+    <a-button>Secondary</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="outline">Outline</a-button>
+    <a-button type="text">Text</a-button>
+  </a-space>
+</template>
+
+<script setup lang="ts">
+import { Message } from '@arco-design/web-vue'
+
+const handleClick = () => {
+  Message.info('hello world!')
+}
+</script>
+
+<style></style>
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      dts: path.resolve(__dirname, 'src/typings', 'auto-imports.d.ts'),
+      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+      resolvers: [ArcoResolver()]
+    }),
+    Components({
+      dts: path.resolve(__dirname, 'src/typings', 'components.d.ts'),
+      resolvers: [ArcoResolver({ sideEffect: true })]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    // host: '0.0.0.0', 需要局域网连接时启用
+    port: 4000
+  }
+})
